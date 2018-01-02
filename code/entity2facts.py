ef8fb65e883063f018a549b31bd00a73faece6aa
Programming Bear <theweijy@gmail.com>
2018-01-02 13:38:11 +0800

    Fix a bug in entity2facts

--- code/entity2facts.py
+++ code/entity2facts.py
@@ -12,7 +12,7 @@ def process(temp):
 
 def genDict():
     dic = {}
-    file_object = open("entityInfo.dat",'r')
+    file_object = open("entityInfo.txt",'r')
     lines = file_object.readlines()
     print("start generating dictionary")
     for line in tqdm(lines):
@@ -24,11 +24,22 @@ def genDict():
         dic[_id] = tuple(temp)
     return dic
     
+def get_max(dic,entities):
+    ans = 0
+    for entity in entities:
+        try:
+            ans = max(ans, dic[entity][1])
+        except:
+            ans = ans
+    return ans
+
 def entity2facts(facts, dic, entity_vocab, relation_vocab, max_mem, entities):
-    answer = np.ones([len(entities), 85059*len(entities), 3])
+    max_num = get_max(dic,entities)
+
+    answer = np.ones([len(entities), max_num*len(entities), 3])
     
     answer[:, :, 0].fill(entity_vocab['DUMMY_MEM'])
-    answer[:, :, 1].fill(entity_vocab['DUMMY_MEM'])
+    answer[:, :, 1].fill(relation_vocab['DUMMY_MEM'])
     answer[:, :, 2].fill(entity_vocab['DUMMY_MEM'])
 
     mem_counter = 0
@@ -36,10 +47,10 @@ def entity2facts(facts, dic, entity_vocab, relation_vocab, max_mem, entities):
         try:
             tu = dic[entity]
         except:
-            print("%d does not exists",entity)
+            print("%d does not exists"%entity)
             continue
-        temp_counter = 0
         for mem_index in xrange(tu[0], tu[0]+tu[1]):
+            print("memory of %d"%mem_counter)
             mem = facts[mem_index]
             e1_int = entity_vocab[mem['e1']] if mem['e1'] in entity_vocab else entity_vocab['UNK']
             e2_int = entity_vocab[mem['e2']] if mem['e2'] in entity_vocab else entity_vocab['UNK']
@@ -50,47 +61,47 @@ def entity2facts(facts, dic, entity_vocab, relation_vocab, max_mem, entities):
             answer[counter][mem_counter][2] = e2_int
 
             mem_counter += 1
-            temp_counter += 1
             if (mem_counter == max_mem):
+                print("over!")
                 break
     return answer
 
 def read_kb_facts():
     facts = []
     #facts_list = defaultdict(list)
-    print('Reading kb file at {}'.format("../kb/freebase.spades.txt"))
-    with open("../kb/freebase.spades.txt") as fb:
+    print('Reading kb file at {}'.format("freebase.spades.txt"))
+    with open("freebase.spades.txt") as fb:
         for counter, line in tqdm(enumerate(fb)):
             line = line.strip()
             line = line[1:-1]
             e1, r1, r2, e2 = [a.strip('\'') for a in [x.strip() for x in line.split(',')]]
             r = r1 + '_' + r2
             facts.append({'e1': e1, 'r': r, 'e2': e2})
-            r = r2 + '_' + r1 
-            facts.append({'e1': e2, 'r': r, 'e2': e1}) # add both 
            # facts_list[e1].append(counter)  # just store the fact counter instead of the fact
     return facts
 
 
 if __name__ == "__main__":
-   # file_object = open("entityInfo.dat",'r')
+   # file_object = open("entityInfo.txt",'r')
    # lines = file_object.readlines()
    # a,b,c = process(lines[0])
    # print(a)
    # print(b)
    # print(c)
-   entity_object = open("../vocab/entity_vocab.json")
-   relation_object = open("../vocab/relation_vocab.json")
+   entity_object = open("entity_vocab.json")
+   relation_object = open("relation_vocab.json")
    entity_voc = json.loads(entity_object.read())
    relation_voc = json.loads(relation_object.read())
 
    facts = read_kb_facts()
    dic = genDict()
     
-   mem = entity2facts(facts, dic, entity_voc, relation_voc, 2147483647, [824729,824730,407779])
-   print(mem[0])
-   print(mem[1])
-   print(mem[2])
+   mem = entity2facts(facts, dic, entity_voc, relation_voc, 2147483647, [730394, 1458844, 1050595, 908309,1789012])
+   print(mem)
+   #print(mem[0])
+   #print(mem[1])
+   #print(mem[2][3])
+   #print(mem[3][4])
    #print(dic[824729])
    #print(dic[407784])
    
